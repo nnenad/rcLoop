3dae7410c04a2c1b72623fb5e4ba78a9b3e788f3
Nenad Nisavic <nenad@sobot.biz>
2014-02-26 15:19:08 +0100

    initial commit

---
+++ myFunctions.js
@@ -0,0 +1,90 @@
+var loop = 0;
+var numberOfLoops = 10;
+var loopInterval;
+colorArray = [];
+
+var colorArea;
+var resultArea;
+var showArrayButton;
+var visualResultArea;
+
+function setUpVars(){
+	colorArea =  document.getElementById("colorArea");
+	resultArea = document.getElementById("resultArea");
+	showArrayButton = document.getElementById("showArrayButton");
+	visualResultArea = document.getElementById("visualResultArea");
+	generateArray();
+}
+function getRandomColor(){
+	var random = Math.random();
+		if(random < 0.25){
+			return "red";
+		}else if(random < 0.5){
+			return "green";
+		}else if(random < 0.75){
+			return "blue";
+		}else{
+			return "black";
+		}
+}
+function generateArray(){
+	colorArray = [];
+	while(colorArray.length < numberOfLoops)
+	{
+		var color = getRandomColor();
+		var lastColor = colorArray[colorArray.length -1];
+		if(lastColor != color){
+			colorArray.push(color);
+		}	
+	}
+}
+function setColorToDiv(loop){
+	colorArea.style.backgroundColor = colorArray[loop];
+}
+
+function runColors(){
+	clearResult();
+	loopInterval = setInterval(function(){
+		startLooping();
+	},1200);
+}
+
+function startLooping(){
+	if(loop < numberOfLoops){
+		setColorToDiv(loop);
+		loop++;
+	}else{
+		visibleArrayButton();
+		clearVars();
+		stopLooping();
+	}
+}
+function clearResult(){
+	showArrayButton.style.visibility = "hidden";
+	resultArea.innerHTML = "";
+}
+
+
+function visibleArrayButton(){
+	showArrayButton.style.visibility = "visible";
+}
+
+function clearVars(){
+	colorArea.style.backgroundColor = "white";
+	loop = 0;
+}
+function stopLooping(){
+	clearInterval(loopInterval);
+}
+
+function showArray(){
+	for(var i =0; i < colorArray.length; i++){
+		var colorDiv=document.createElement("Div");
+		colorDiv.style.backgroundColor = colorArray[i];
+		colorDiv.style.width = "15px";
+		colorDiv.style.height = "15px";
+		colorDiv.style.display = "inline-block";
+		visualResultArea.appendChild(colorDiv);
+	}
+	resultArea.innerHTML = colorArray;
+}
